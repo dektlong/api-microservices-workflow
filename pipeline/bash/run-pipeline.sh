5939ef2a340fcb0411482b42e1541cd2e6c38efd
dektlong <dekel.tankel@gmail.com>
2021-06-11 17:07:25 -0700

    Update run-pipeline.sh

--- pipeline/bash/run-pipeline.sh
+++ pipeline/bash/run-pipeline.sh
@@ -1,104 +1,33 @@
 #!/usr/bin/env bash
 
-#################### functions #######################
-
-#deploy-backend 
-deploy-backend() {
-	
-    echo
-    echo "=========> Invoke image build (if needed)..."
-    echo
-    kp image patch $BACKEND_TBS_IMAGE -n $APP_NAMESPACE
-    echo
-    kp build list {{projectName}-backend-image -n $APP_NAMESPACE
-    
-    echo
-    echo "=========> Apply backend app, service and routes ..."
-    echo    
-    #kubectl set image deployment/dekt4pets-backend dekt4pets-backend=$IMG_REGISTRY_URL/$IMG_REGISTRY_APP_REPO/$BACKEND_TBS_IMAGE:$APP_VERSION -n $APP_NAMESPACE
-    kustomize build workload-backend | kubectl apply -f -
-    
-}
-
-#deploy-frontend 
-deploy-frontend() {
-	
-    echo
-    echo "=========> Apply frontend app, service and routes ..."
-    echo
-	
-    kustomize build workload-frontend | kubectl apply -f -
-
-}
-
-#patch-backend
-patch-backend() {
-    
-    git_commit_msg="add check-adopter api"
-    
-    echo
-    echo "=========> Commit code changes to $DEMO_APP_GIT_REPO  ..."
-    echo
-    git-push "$git_commit_msg"
-    
-    echo
-    echo "=========> Auto-build $BACKEND_TBS_IMAGE image on latest git commit (commit: $_latestCommitId) ..."
-    echo
-    
-    kp image patch $BACKEND_TBS_IMAGE --git-revision $_latestCommitId -n $APP_NAMESPACE
-    
-    echo
-    echo "Waiting for next github polling interval ..."
-    echo  
-    
-    sleep 10 #enough time, instead of active polling which is not recommended by the TBS team
-    
-    kp build list $BACKEND_TBS_IMAGE -n $APP_NAMESPACE
-
-    #kp build status $BACKEND_TBS_IMAGE -n $APP_NAMESPACE
-
-    echo
-    echo "Starting to tail build logs ..."
-    echo
-    
-    kp build logs $BACKEND_TBS_IMAGE -n $APP_NAMESPACE
-    
-    echo
-    echo "=========> Apply changes to backend app, service and routes ..."
-    echo
-    
-    #workaround for image refresh issue
-    kubectl delete -f workload-backend/dekt4pets-backend-app.yaml -n $APP_NAMESPACE > /dev/null 
-        
-    kustomize build workload-backend | kubectl apply -f -
-
-}
-
-#delete-workloads
-delete-workloads() {
-
-    echo
-    echo "=========> Remove frontend and backend workloads..."
-    echo
-
-    kustomize build workload-backend | kubectl delete -f -
-
-    kustomize build workload-frontend | kubectl delete -f -    
-
-}
+#simple pipeline to deploy app components, gateways and routes using Tanzu Build Service, Spring Cloud Gateway and Kustomize
 
 case $1 in
 deploy)
-  kp image patch {{projectName}-image
+  kp image create {{microserviceOneName}}-image -n {{appName}}-ns \
+        --tag goharbor.io/{{appName}}/{{microserviceOneName}}:0.0.1 \
+        --git https://github.com/myuser/{{microserviceOneName}}" \
+        --git-revision main \
+	--builder {{appName}}-builder \
+        --wait
+  kp image create {{microserviceTwoName}}-image -n {{appName}}-ns \
+        --tag goharbor.io/{{appName}}/{{microserviceTwoName}}:0.0.1 \
+        --git https://github.com/myuser/{{microserviceTwoName}}" \
+        --git-revision main \
+	--builder {{appName}}-builder \
+        --wait
   kustomize build . | kubectl apply -f -
   ;;
-deploy-frontend)
-  deploy-frontend
+patch)
+  kp image patch {{microserviceOneName}}-image
+  kp image patch {{microserviceTwoName}}-image
+  kustomize build . | kubectl apply -f -
   ;;
-patch-backend)
-  patch-backend
-  ;;
+delete)
+  kustomize build . | kubectl delete -f -
+  kp image delete {{microserviceOneName}}-image
+  kp image delete {{microserviceTwoName}}-image
 *)
-  usage
+  # incorrect usage
   ;;
 esac
